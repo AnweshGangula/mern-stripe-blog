966b0ec42d8badb9db822ae99d8c8345f6fa218a
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-04-03 09:44:10 +0530

    added frontend form to create post

---
+++ frontend/src/Components/Posts/CreatePost.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+function CreatePost() {
+
+    const formik = useFormik({
+        // initial data
+        initialValues: {
+            title: '',
+            description: ''
+        },// validation
+        validationSchema: Yup.object({
+            title: Yup.string().required('Title is required'),
+            description: Yup.string().required('Description is required')
+        }),
+        // submit handler
+        onSubmit: (values) => {
+            console.log({ values })
+        }
+    })
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <input type="text" name="title" id="postTitle" placeholder='Enter Post Title'
+                    {...formik.getFieldProps('title')}
+                />
+                {/* display error message */}
+                {formik.touched.title && formik.errors.title &&
+                    <span>
+                        {formik.errors.title}
+                    </span>
+                }
+                <input type="text" name="description" id="postDescription" placeholder='Enter Post Description...'
+                    {...formik.getFieldProps('description')}
+                />
+                {/* display error message */}
+                {formik.touched.description && formik.errors.description &&
+                    <span>
+                        {formik.errors.description}
+                    </span>
+                }
+                <button type="submit">
+                    Create Post
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default CreatePost
